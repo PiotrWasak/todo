2888a0f6a012a8fd9cbb8b742b8c9fffe1582fc5
PiotrWasak <68294222+PiotrWasak@users.noreply.github.com>
2021-10-16 22:16:44 +0200

    Publish
    TODO

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <TODO />
+  </div>
+</template>
+
+<script>
+import TODO from "./components/TODO.vue";
+
+export default {
+  name: "App",
+  components: {
+    TODO,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/TODO.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="todo container">
+    <h1>TODO</h1>
+    <br /><br /><br />
+
+    <label for="taskInput" class="form-label lead">Dodaj zadanie</label>
+    <input
+      @keyup.enter="addTask"
+      v-model="task"
+      type="text"
+      class="form-control"
+      id="taskInput"
+    />
+    <br />
+    <button @click="addTask" type="submit" class="btn btn-primary">
+      Submit
+    </button>
+    <br />
+    <ul class="list-group">
+      <li
+        v-for="(task, index) in tasks"
+        :key="task.id"
+        class="list-group-item list-group-item-action d-flex justify-content-between align-items-center"
+      >
+        {{ task }}
+        <span @click="deleteTask(index)" class="badge badge-danger badge-pill">
+          <i class="fas fa-times-circle"></i
+        ></span>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TODO",
+  data() {
+    return {
+      tasks: ["Create TODO app"],
+      task: "",
+    };
+  },
+  props: {},
+  methods: {
+    addTask() {
+      if (this.task) {
+        this.tasks.push(this.task);
+      }
+    },
+    deleteTask(taskIndex) {
+      this.tasks.splice(taskIndex, 1);
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+fas {
+  float: right;
+}
+</style>
